6fae90f88ec0064084d6d93ec62df63370395ab9
Craig Phillips <iwonbigbro@gmail.com>
2015-08-13 02:02:16 +0100

    Adding a test for escape warnings.
    Fixed colour output when no tty present.

---
+++ t/test_escape_sequence_warning.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export PATH=$PATH:$(readlink -m "$BASH_SOURCE/../../bin")
+
+script=$(mktemp)
+trap "rm -f $script" EXIT
+
+cat >$script <<'SCRIPT'
+#!/bin/bash
+
+# Copyright (C) 2015 Craig Phillips.  All rights reserved.
+
+myprogram=$(readlink -f "$BASH_SOURCE")
+
+echo "an invalid escape sequence"
+
+exit 0
+SCRIPT
+
+cat >$script.e <<EXPECTED
+$script:7:8 warning: unexpected character in escape sequence
+  echo "^[an invalid escape sequence"  
+  ........^
+EXPECTED
+
+bash ${bash_opts:-} bashpp $script 1>/dev/null 2>$script.a </dev/null
+diff -U3 $script.e $script.a
