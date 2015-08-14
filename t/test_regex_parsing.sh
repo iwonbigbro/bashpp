a33433b70d024b8a70a2f8d51bb911f6aa4b005a
Craig Phillips <craig.phillips@cdk.com>
2015-08-14 13:24:55 +0100

    Fix broken test.

--- t/test_regex_parsing.sh
+++ t/test_regex_parsing.sh
@@ -9,6 +9,8 @@ trap "rm -f $script" EXIT
 
 VERBOSE=0
 
+exec </dev/null
+
 cat >$script <<'SCRIPT'
 #!/bin/bash
 
@@ -19,9 +21,15 @@ elif [[ $myvar =~ ^a simple\ regex$ ]] ; then
 fi
 SCRIPT
 
-cat >$script.err_e <<'OUTPUT'
+cat >$script.err_e <<OUTPUT
+$script:5:24 error: 1: unexpected characters in conditional: 'si'
+  elif [[ \$myvar =~ ^a simple\\ regex\$ ]] ; then  
+  .......................^
+$script:5:36 error: 2: unexpected token in parameter expansion
+  elif [[ \$myvar =~ ^a simple\\ regex\$ ]] ; then  
+  ...................................^
 OUTPUT
 
-bash ${bash_opts:-} bashpp $script -o $script.a 2>&1 | tee $script.err_a
+bash ${bash_opts:-} bashpp $script -o $script.a 2>&1 | tee $script.err_a || true
 diff -U3 $script $script.a
 diff -U3 $script.err_e $script.err_a
